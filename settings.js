ebf8f5af964d4be5ad7b40568557a2d532eb4fdf
ALiYTMAX <134224451+DARKWORLDmax@users.noreply.github.com>
2023-12-21 20:21:28 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //aumto presence update
-global.autoReading = false //auto recording (true to on, false to off)
-global.autoTyping = false //auto tying (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoReading = true //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.autoblockmorroco = false //auto block 212 (true to on, false to off)
 global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
 global.antispam = false //auto kick spammer (true to on, false to off)
@@ -22,26 +22,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.ownernomer = "923138164795" //ur owner number2
+global.ownername = "🦄Zain Office Bot" //ur owner name
+global.ytname = "YT: 3DiWorld" //ur yt chanel name
+global.socialm = "GitHub: DARKWORLDmax" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V9"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtube.com/@DGXeon"
-global.wagc = "https://chat.whatsapp.com/Kjm8rnDFcpb04gQNSTbW2d"
-global.themeemoji = '🪀'
+global.botname = "DARK Web WORLD"
+global.ownernumber = '923138164795'
+global.ownername = 'Zain Ahmed'
+global.ownerNumber = ["923138164795@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@3DiWorld"
+global.websitex = "https://youtube.com/@3DiWorld"
+global.wagc = "https://chat.whatsapp.com/BbkrFu9FSac3pBa2PIoV0d"
+global.themeemoji = '🌹'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "DarkWorld Stickers"
+global.creator = "923138164795@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
